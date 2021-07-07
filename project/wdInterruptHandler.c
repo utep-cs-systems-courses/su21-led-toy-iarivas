e195ea03fa9ef1da42a0111c2a5be083c516ccb8
iarivas <iarivas@miners.utep.edu>
2021-07-06 23:30:24 -0600

    Fixed Looping Error

--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -9,14 +9,17 @@ static char count = 0;
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
 
-  if(count == 4){ count = 0;}
-  if(time == 250){ time = 0; count++; }
-  if(++blink_count == 1 && count == 0){
-    red_dim(0); time++; blink_count = 0; }
-  else if(blink_count == 2 && count == 1){
-    red_dim(1); time++; blink_count = 0; }
-  else if(blink_count == 1 && count == 2){
-    red_dim(2); time++; blink_count = 0; }
-  else if(blink_count == 2 && count == 3){
-    red_dim(3); time++; blink_count = 0; }
+  if(time == 249){ time = 0; count++;}
+
+  if(count == 4){ count = 0; }
+  else{
+    if(++blink_count == 1 && count == 0){
+      red_dim(0); time++; blink_count = 0; }
+    else if(blink_count == 2 && count == 1){
+      red_dim(1); time++; blink_count = 0; }
+    else if(blink_count == 1 && count == 2){
+      red_dim(2); time++; blink_count = 0; }
+    else if(blink_count == 2 && count == 3){
+      red_dim(3); time++; blink_count = 0; }
+  }
 }
